7741af7da9979ce0cef359232ed022199fcb32d9
tallybh <tallybh@yahoo.com>
2025-03-20 11:19:51 +0200

    Add files via upload

---
+++ Day 5/Rabbit example/Rabbit example/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Rabbit_example.Services;
+
+namespace Rabbit_example.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMessanger _messanger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMessanger messanger)
+        {
+            _messanger = messanger;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            
+            var retVal =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            await _messanger.SendMessage(retVal);
+
+            return retVal;
+        }
+    }
+}
---
+++ Day 5/Rabbit example/Rabbit example/Services/RabbitMessageProducer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Rabbit_example.Services
+{
+    public class RabbitMessager : IMessanger
+    {
+        public async Task SendMessage<T>(T message)
+        {
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            var connection = await factory.CreateConnectionAsync();
+            using
+            var channel = await connection.CreateChannelAsync();
+            await channel.QueueDeclareAsync("product", exclusive: false);
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            await channel.BasicPublishAsync(exchange: "", routingKey: "product", body: body);
+        }
+
+
+    }
+}
